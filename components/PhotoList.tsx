a37521be0725f3805ef28354c2251b702bd842c9
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-08-26 13:23:13 -0400

    turned into list

--- components/PhotoList.tsx
+++ components/PhotoList.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 import { UnsplashPhoto } from "../unplash/types";
 import Photo from "./Photo";
+import styles from "../styles/photos.module.css";
 
 const PhotoList: React.FC<{ photos: UnsplashPhoto[] }> = ({ photos }) => {
   return (
-    <div className="text-orange-700">
-      <div className="text-3xl">hi</div>
+    <div className={styles.photoList}>
       {photos?.map((photo: UnsplashPhoto) => (
-        <p key={photo.id}>
-          <Photo photo={photo}></Photo>
-        </p>
+        <Photo photo={photo}></Photo>
       ))}
     </div>
   );
